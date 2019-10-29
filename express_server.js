fb926caf55dae76a6bc2f1321a38a7da6873557c
nolan <nolan796@live.ca>
2019-10-29 10:50:01 -0600

    Add functional delete button / created index redirection when submitting long url.

--- express_server.js
+++ express_server.js
@@ -17,13 +17,16 @@ app.get("/urls/new", (req, res) => {
     res.render("urls_new");
 });
 app.get("/urls/:shortURL", (req, res) => {
-    let templateVars = { shortURL: req.params.shortURL, longURL: req.params.longURL };
-    res.render("urls_show", templateVars);
+  const longURL = urlDatabase[req.params.shortURL]
+  let templateVars = { 
+    shortURL: req.params.shortURL,
+    longURL: longURL
+  };
+  res.render("urls_show", templateVars);
 });
 app.get("/urls.json", (req, res) => {
     res.json(urlDatabase);
   json.stringify("/urls")
-  
 });
 app.get("/", (req, res) => {
     res.send("Hello!");
@@ -37,17 +40,33 @@ app.listen(PORT, () => {
 app.post("/urls", (req, res) => {
     let long = req.body.longURL;  // Log the POST request body to the console
     const rand = generateRandomString();
-    urlDatabase[rand] = 'http://'+long;
-    res.redirect('u/' + rand);         // Respond with 'Ok' (we will replace this)
+    urlDatabase[rand] = 'http://' + long;
+    res.redirect('urls/' + rand);         // Respond with 'Ok' (we will replace this)
  });
 app.get("/u/:shortURL", (req, res) => {
-    console.log(urlDatabase, req.params);
     const longURL = urlDatabase[req.params.shortURL];
     res.redirect(longURL);
-});
+    // res.redirect('/url');
+  });
+
+app.post("/urls/:shortURL/delete", (req, res) => {
+  delete urlDatabase[req.params.shortURL];
+  console.log('its type is: ' + typeof req.params.shortURL + ' and its property name is: ' + req.params.shortURL)
+  res.redirect("/url")
+}); 
+
+app.get("/url", (req, res) => {
+res.redirect('/urls')
+})
+
+
+
+
+
+
 const generateRandomString = function() {
   let random = Math.random().toString(36).substring(7);
-  return random;
-};
+ return random;
+  };
    
  
